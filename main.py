80f99d8375e1178e05838d198a85621f11b4f73f
Brendon O'Laney <Brendon.O'Laney@gov.bc.ca>
2024-12-12 13:41:44 -0800

    Implement weight application

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from utils.forecast import fetch_and_clean_data
+from utils.weights import apply_weights
 
 
 def main():
     forecast_data = fetch_and_clean_data()
+    weighted_forecast_data = apply_weights(forecast_data)
 
 
 if __name__ == "__main__":
